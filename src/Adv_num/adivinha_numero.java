d20720c6dddd2f2ae20bed0970ed3fe801e25ec9
FelipeMarani <felipejesusdasilva02@gmail.com>
2024-03-21 19:54:05 -0300

    update

---
+++ src/Adv_num/adivinha_numero.java
@@ -0,0 +1,37 @@
+package Adv_num;
+
+import java.util.Scanner;
+
+public class adivinha_numero {
+	
+	public static void main(String[] args) {
+		
+		Scanner tcd = new Scanner(System.in);
+		
+		int numSecreto = (int)(Math.random()*100);
+		
+		System.out.println("Digite um número para adivinhar");
+		
+		int numDigitado = tcd.nextInt();
+		
+		boolean isCorrect  = false;
+		while (!isCorrect) {
+			
+			if(numDigitado > numSecreto) {
+				System.out.println("O número secreto é menor");
+			}
+			
+			if(numDigitado < numSecreto) {
+				System.out.println("O número secreto é maior");
+			}
+			if(numDigitado == numSecreto) {
+				System.out.println("Você ganhou seu puto");
+				isCorrect = true;
+				}
+			numDigitado =tcd.nextInt();
+		}
+			
+		}
+	
+}
+
